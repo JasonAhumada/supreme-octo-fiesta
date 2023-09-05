5e50a1141350caf910ae6e6df3eca1562c786521
JasonAhumada <a1746103@adelaide.edu.au>
2023-09-05 15:29:35 +0930

    address issue

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -27,7 +27,37 @@ def make_positions():
     for i in range(NSRC):
         ras.append(ra + uniform(-1,1))
         decs.append(dec + uniform(-1,1))
+
+    # apply our filter
+    ras, decs = crop_to_circle(ras,decs)
+
     return ras, decs
+
+def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
+    """
+    Crop an input list of positions so that they lie within radius of
+    a reference position
+
+    Parameters
+    ----------
+    ras,decs : list(float)
+        The ra and dec in degrees of the data points
+    ref_ra, ref_dec: float
+        The reference location
+    radius: float
+        The radius in degrees
+    Returns
+    -------
+    ras, decs : list
+        A list of ra and dec coordinates that pass our filter.
+    """
+    ra_out = []
+    dec_out = []
+    for i in range(len(ras)):
+        if (ras[i]-ref_ra)**2 + (decs[i]-ref_dec)**2 < radius**2:
+            ra_out.append(ras[i])
+            dec_out.append(ras[i])
+    return ra_out, dec_out
 
 # now write these to a csv file for use by my other program
 def save_positions(ras, decs):
@@ -39,9 +69,6 @@ def save_positions(ras, decs):
 def main():
     ras, decs = make_positions()
     save_positions(ras, decs)
-
-def clip_to_radius():
-    return
 
 if __name__ == "__main__":
     main()
